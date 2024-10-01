0b9e3df7a7449fcac0a9efe25e97b26dc3d50bea
Daniel-A-Mendoza <122234952+Daniel-A-Mendoza@users.noreply.github.com>
2024-10-01 13:38:06 -0700

    Delete BasicAutomata.java
    An DFA is a form of NFA, so maybe Polymorphism will work for the Transition Function

--- BasicAutomata.java
+++
@@ -1,9 +0,0 @@
-// We will list all the basic methods used in both types of Automata
-public interface BasicAutomata <E> {
-    public void setInitialState(E state); // This method will set the initial state of the automata
-    public E getInitialState(); // This method will return the initial state of the automata
-    public void setFinalState(E state); // This method will set the final state of the automata
-    public E getFinalState(); // This method will return the final state of the automata
-    public char getTransition(E state, char symbol); // This method will return the transition of the automata
-    public boolean isFinalState(E state); // This method will return true if the current state is in the final state.
-}
