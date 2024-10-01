928e21c8a841f971b2378884e9706b2ea715d97d
Daniel-A-Mendoza <dmendoza.alfredo@gmail.com>
2024-10-01 13:55:42 -0700

    Created Documentation for State Class

--- State.java
+++ State.java
@@ -1,12 +1,24 @@
 public class State {
+    // States contain the following characteristics: name, isFinal, isInitial
     private String name;
     private boolean isFinal;
     private boolean isInitial;
+
+    // Empty State or Default Constructor
+    public State() {
+        name = "";
+        isFinal = false;
+        isInitial = false;
+    }
+
+    // Creating a State with information about its characteristics
     public State(String name, boolean isFinal, boolean isInitial) {
         this.name = name;
         this.isFinal = isFinal;
         this.isInitial = isInitial;
     }
+
+    // Getters
     public String getName() {
         return name;
     }
@@ -16,6 +28,8 @@ public class State {
     public boolean isInitial() {
         return isInitial;
     }
+
+    // Setters
     public void setName(String name){
         this.name = name;
     }
@@ -25,16 +39,24 @@ public class State {
     public void setInitial(boolean isInitial) {
         this.isInitial = isInitial;
     }
+
+    // Overriding the toString method and returning characteristics about the State
     public String toString() {
-        return name;
+        return "State Name: " + name + "\nFIs Final State?: " + isFinal + "\nIs Initial State?: " + isInitial;
     }
-    public boolean equals(State comperand) {
-        if (comperand == null) {
+
+    // Overriding the equals method to compare two states
+    public boolean equals(State operand) {
+        // If the operand is null, that means they cannot be the same state since one of them does not exist
+        if (operand == null) {
             return false;
         }
-        if (comperand == this) {
+        // If the operand is the same as the current state, then they are the same state
+        if (operand == this) {
             return true;
         }
-        return name.equals(comperand.name);
+        // If the operand is not the same instance, but we compare that they have the same name, they are the same state
+        // We will use this case when we create a temporary State object but it has the same name as the original, and we want to compare
+        return name.equals(operand.name);
     }
 }
