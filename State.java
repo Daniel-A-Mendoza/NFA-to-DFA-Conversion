5edf5a5a330e41c3b3cbbd0acc167acb87784e09
Daniel-A-Mendoza <dmendoza.alfredo@gmail.com>
2024-10-03 21:39:32 -0700

    State Class now has 4 fields including the transitions. Adding functions to easily add and remove transitions.

--- State.java
+++ State.java
@@ -1,21 +1,40 @@
+// Let's great a Hashmap to store the symbol (key) and the list of states (value) that the current state can go to using the symbol
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
 public class State {
     // States contain the following characteristics: name, isFinal, isInitial
     private String name;
     private boolean isFinal;
     private boolean isInitial;
-
+    HashMap <Character, State[]> transitionFunction;
     // Empty State or Default Constructor
     public State() {
         name = "";
         isFinal = false;
         isInitial = false;
+        transitionFunction = new HashMap<>();
+    }
+    // Creating a State with just the Name
+    public State(String name){
+        this.name = name;
+        isFinal = false;
+        isInitial = false;
+        transitionFunction = new HashMap<>();
     }
 
     // Creating a State with information about its characteristics
-    public State(String name, boolean isFinal, boolean isInitial) {
+    public State(String name, boolean isFinal, boolean isInitial, HashMap<Character, State[]> transitionFunction){
         this.name = name;
         this.isFinal = isFinal;
         this.isInitial = isInitial;
+        if (transitionFunction == null){
+            this.transitionFunction = new HashMap<>();
+        }
+        else{
+        this.transitionFunction = transitionFunction;
+        }
     }
 
     // Getters
@@ -27,8 +46,10 @@ public class State {
     }
     public boolean isInitial() {
         return isInitial;
+    } 
+    public HashMap<Character, State[]> getTransitionFunction(){
+        return transitionFunction;
     }
-
     // Setters
     public void setName(String name){
         this.name = name;
@@ -39,11 +60,81 @@ public class State {
     public void setInitial(boolean isInitial) {
         this.isInitial = isInitial;
     }
+    public void setTransitionFunction(HashMap<Character, State[]> transitionFunction){
+        this.transitionFunction = transitionFunction;
+    }
+
+    // Add a Transition
+    public void addTransition(Character symbol, State[] states){
+        // If the symbol is not in the transition function, we will add it
+        if (!transitionFunction.containsKey(symbol)){
+            transitionFunction.put(symbol, states);
+        }
+        // If the symbol is in the transition function, we will add the state to the list of states that the current state can go to using the symbol
+        else{
+            int tempLength = states.length;
+            State[] temp = transitionFunction.get(symbol);
+            State[] temp2 = new State[temp.length + tempLength];
+            for (int i = 0; i < temp.length; i++){
+                temp2[i] = temp[i];
+            }
+            for (int i = temp.length; i < tempLength; i++){
+                temp2[i] = temp[i];
+            }
+            transitionFunction.put(symbol, temp2);
+        }
+    }
+
+    // Remove a Transition
+    public void removeTransition(Character symbol, State[] states){
+        // If the symbol is not in the transition function, we will not remove anything
+        if (!transitionFunction.containsKey(symbol)){
+            System.out.println("Symbol does not exist in the Transition Function");
+        }
+        // If the symbol is in the transition function, we will remove the state from the list of states that the current state can go to using the symbol
+        else{
+            ArrayList<State> arrayListTemp = new ArrayList<>();
+            State[] temp = new State[transitionFunction.get(symbol).length - states.length];
+            for (int i = 0; i < transitionFunction.get(symbol).length; i++){
+                boolean found = false;
+                for (int j = 0; j < states.length; j++){
+                    if (transitionFunction.get(symbol)[i] == states[j]){
+                        found = true;
+                    }
+                }
+                if(!found){
+                    arrayListTemp.add(transitionFunction.get(symbol)[i]);
+                }
+            }
+           arrayListTemp.toArray(temp);
+        }
+    }
+
 
     // Overriding the toString method and returning characteristics about the State
     public String toString() {
-        return "State Name: " + name + "\nFIs Final State?: " + isFinal + "\nIs Initial State?: " + isInitial;
-    }
+        String result = "State: " + name + "\n" + "Is Initial: " + isInitial + "\n" + "Is Final: " + isFinal + "\n" + "Transition Function: ";
+        if (transitionFunction.isEmpty()){
+            result += "Empty";
+        }
+        else{
+            for (Map.Entry<Character, State[]> entry : transitionFunction.entrySet()){
+                result += "δ(" ;
+                result += entry.getKey().toString() + ", {";
+                State firstState = entry.getValue()[0];
+               
+                for (State state : entry.getValue()) {
+                    if (!state.equals(firstState)){
+                        result += ",";
+                    }
+                        result += state.getName();
+                    }
+                    
+                }
+                result += "})\n";
+            }
+            return result;
+        }
 
     // Overriding the equals method to compare two states
     public boolean equals(State operand) {
